ab6fb59a06020114744e5d83b53401ec9e7830d1
shgupta1 <shgupta@grepruby.io>
2022-04-30 17:34:54 +0530

    feat: added constants

---
+++ blog_app_frontend/src/utils/constants/index.js
@@ -0,0 +1,9 @@
+export const TOKEN = 'authToken'
+
+export const ROUTE_PATHS = {
+  home: '/',
+  dashboard: '/dashboard',
+  login: 'login',
+  register: '/register',
+  createPost: '/create-post',
+}
